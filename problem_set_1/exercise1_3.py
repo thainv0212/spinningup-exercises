dd849552e50ac65a5306baaefbc8ecf2e65d75d4
Thai <nguyenvanthai0212@gmail.com>
2019-09-24 11:32:37 +0700

    update

--- problem_set_1/exercise1_3.py
+++ problem_set_1/exercise1_3.py
@@ -22,6 +22,7 @@ TD3 updates.
 
 """
 
+
 class ReplayBuffer:
     """
     A simple FIFO experience replay buffer for TD3 agents.
@@ -41,8 +42,8 @@ class ReplayBuffer:
         self.acts_buf[self.ptr] = act
         self.rews_buf[self.ptr] = rew
         self.done_buf[self.ptr] = done
-        self.ptr = (self.ptr+1) % self.max_size
-        self.size = min(self.size+1, self.max_size)
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
 
     def sample_batch(self, batch_size=32):
         idxs = np.random.randint(0, self.size, size=batch_size)
@@ -52,15 +53,18 @@ class ReplayBuffer:
                     rews=self.rews_buf[idxs],
                     done=self.done_buf[idxs])
 
+
 """
 
 TD3 (Twin Delayed DDPG)
 
 """
-def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0, 
-        steps_per_epoch=5000, epochs=100, replay_size=int(1e6), gamma=0.99, 
-        polyak=0.995, pi_lr=1e-3, q_lr=1e-3, batch_size=100, start_steps=10000, 
-        act_noise=0.1, target_noise=0.2, noise_clip=0.5, policy_delay=2, 
+
+
+def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
+        steps_per_epoch=5000, epochs=100, replay_size=int(1e6), gamma=0.99,
+        polyak=0.995, pi_lr=1e-3, q_lr=1e-3, batch_size=100, start_steps=10000,
+        act_noise=0.1, target_noise=0.2, noise_clip=0.5, policy_delay=2,
         max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
     """
 
@@ -161,11 +165,11 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
     # Inputs to computation graph
     x_ph, a_ph, x2_ph, r_ph, d_ph = core.placeholders(obs_dim, act_dim, obs_dim, None, None)
 
-    #=========================================================================#
+    # =========================================================================#
     #                                                                         #
     #           All of your code goes in the space below.                     #
     #                                                                         #
-    #=========================================================================#
+    # =========================================================================#
 
     # Main outputs from computation graph
     with tf.variable_scope('main'):
@@ -176,7 +180,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         #######################
         pi, q1, q2, q1_pi = actor_critic(x_ph, a_ph, **ac_kwargs)
         pass
-    
+
     # Target policy network
     with tf.variable_scope('target'):
         #######################
@@ -184,9 +188,9 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         #   YOUR CODE HERE    #
         #                     #
         #######################
-        pi_targ,_,_,_ = actor_critic(x2_ph, a_ph, **ac_kwargs)
+        pi_targ, _, _, _ = actor_critic(x2_ph, a_ph, **ac_kwargs)
         pass
-    
+
     # Target Q networks
     with tf.variable_scope('target', reuse=True):
 
@@ -207,7 +211,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         a2 = pi_targ + epsilon
         a2 = tf.clip_by_value(a2, -act_limit, act_limit)
         _, q1_tar, q2_tar, _ = actor_critic(x2_ph, a2, **ac_kwargs)
-        
+
         pass
 
     # Experience buffer
@@ -215,7 +219,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
 
     # Count variables
     var_counts = tuple(core.count_vars(scope) for scope in ['main/pi', 'main/q1', 'main/q2', 'main'])
-    print('\nNumber of parameters: \t pi: %d, \t q1: %d, \t q2: %d, \t total: %d\n'%var_counts)
+    print('\nNumber of parameters: \t pi: %d, \t q1: %d, \t q2: %d, \t total: %d\n' % var_counts)
 
     # Bellman backup for Q functions, using Clipped Double-Q targets
     #######################
@@ -240,11 +244,11 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
     q1_loss = tf.reduce_mean((q1 - backup) ** 2)
     q2_loss = tf.reduce_mean((q2 - backup) ** 2)
     q_loss = q1_loss + q2_loss
-    #=========================================================================#
+    # =========================================================================#
     #                                                                         #
     #           All of your code goes in the space above.                     #
     #                                                                         #
-    #=========================================================================#
+    # =========================================================================#
 
     # Separate train ops for pi, q
     pi_optimizer = tf.train.AdamOptimizer(learning_rate=pi_lr)
@@ -253,12 +257,12 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
     train_q_op = q_optimizer.minimize(q_loss, var_list=get_vars('main/q'))
 
     # Polyak averaging for target variables
-    target_update = tf.group([tf.assign(v_targ, polyak*v_targ + (1-polyak)*v_main)
+    target_update = tf.group([tf.assign(v_targ, polyak * v_targ + (1 - polyak) * v_main)
                               for v_main, v_targ in zip(get_vars('main'), get_vars('target'))])
 
     # Initializing targets to match main variables
     target_init = tf.group([tf.assign(v_targ, v_main)
-                              for v_main, v_targ in zip(get_vars('main'), get_vars('target'))])
+                            for v_main, v_targ in zip(get_vars('main'), get_vars('target'))])
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
@@ -268,14 +272,14 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
     logger.setup_tf_saver(sess, inputs={'x': x_ph, 'a': a_ph}, outputs={'pi': pi, 'q1': q1, 'q2': q2})
 
     def get_action(o, noise_scale):
-        a = sess.run(pi, feed_dict={x_ph: o.reshape(1,-1)})[0]
+        a = sess.run(pi, feed_dict={x_ph: o.reshape(1, -1)})[0]
         a += noise_scale * np.random.randn(act_dim)
         return np.clip(a, -act_limit, act_limit)
 
     def test_agent(n=10):
-        for j in range(n):
+        for _ in range(n):
             o, r, d, ep_ret, ep_len = test_env.reset(), 0, False, 0, 0
-            while not(d or (ep_len == max_ep_len)):
+            while not (d or (ep_len == max_ep_len)):
                 # Take deterministic actions at test time (noise_scale=0)
                 o, r, d, _ = test_env.step(get_action(o, 0))
                 ep_ret += r
@@ -307,7 +311,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
         # Ignore the "done" signal if it comes from hitting the time
         # horizon (that is, when it's an artificial terminal signal
         # that isn't based on the agent's state)
-        d = False if ep_len==max_ep_len else d
+        d = False if ep_len == max_ep_len else d
 
         # Store experience to replay buffer
         replay_buffer.store(o, a, r, o2, d)
@@ -329,7 +333,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
                              a_ph: batch['acts'],
                              r_ph: batch['rews'],
                              d_ph: batch['done']
-                            }
+                             }
                 q_step_ops = [q_loss, q1, q2, train_q_op]
                 outs = sess.run(q_step_ops, feed_dict)
                 logger.store(LossQ=outs[0], Q1Vals=outs[1], Q2Vals=outs[2])
@@ -347,7 +351,7 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
             epoch = t // steps_per_epoch
 
             # Save model
-            if (epoch % save_freq == 0) or (epoch == epochs-1):
+            if (epoch % save_freq == 0) or (epoch == epochs - 1):
                 logger.save_state({'env': env}, None)
 
             # Test the performance of the deterministic version of the agent.
@@ -364,11 +368,13 @@ def td3(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
             logger.log_tabular('Q2Vals', with_min_and_max=True)
             logger.log_tabular('LossPi', average_only=True)
             logger.log_tabular('LossQ', average_only=True)
-            logger.log_tabular('Time', time.time()-start_time)
+            logger.log_tabular('Time', time.time() - start_time)
             logger.dump_tabular()
+
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='HalfCheetah-v2')
     parser.add_argument('--seed', '-s', type=int, default=0)
@@ -377,18 +383,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     from spinup.utils.run_utils import setup_logger_kwargs
+
     logger_kwargs = setup_logger_kwargs(args.exp_name + '-' + args.env.lower(), args.seed)
 
     all_kwargs = dict(
-        env_fn=lambda : gym.make(args.env), 
+        env_fn=lambda: gym.make(args.env),
         actor_critic=core.mlp_actor_critic,
-        ac_kwargs=dict(hidden_sizes=[128,128]), 
+        ac_kwargs=dict(hidden_sizes=[128, 128]),
         max_ep_len=150,
-        seed=args.seed, 
+        seed=args.seed,
         logger_kwargs=logger_kwargs,
         epochs=10
-        )
-    
+    )
+
     if args.use_soln:
         true_td3(**all_kwargs)
     else:
